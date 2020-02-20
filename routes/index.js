983d6d10486c29ba2e34746ce00ebc548627d3b3
Jeffrey Lau <j8lau@ucsd.edu>
2020-02-20 13:20:29 -0800

    finished

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ projects["viewAlt"] = false;
  * GET home page.
  */
 
-exports.view = function(request, response){
+exports.view = function(request, response) {
 	projects["viewAlt"] = false;
   	response.render('index', projects);
 };
 
 exports.viewAlt = function(request, response) {
 	projects["viewAlt"] = true;
-	response.render('index', projects)
+	response.render('index', projects);
 };
